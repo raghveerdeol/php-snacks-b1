9d4f4e7487903a5af149f972a4bfaa4b9303a94a
raghveerdeol <deolraghveer5@gmail.com>
2024-06-26 18:07:01 +0200

    aggiunto null coalescing operator

--- snack 2/index.php
+++ snack 2/index.php
@@ -1,8 +1,11 @@
 <!-- Snack 2
 Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
- che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
- Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
-    <!DOCTYPE html>
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+<?php
+$_GET['nome'] = $_GET['nome'] ?? '';
+?>
+<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -12,14 +15,17 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
     <body>
         <main>
             <form action="./index.php" method="get">
+                <label for="nome">Inserisci il nome: </label>
                 <input type="text" name="nome" id="nome">
+                <label for="email">Inserisci l'email: </label>
                 <input type="email" name="email" id="email">
+                <label for="age">Inserisci l'età: </label>
                 <input type="number" name="age" id="age">
                 <input type="submit">
             </form>
             <p>
             <?php
-                if ((strlen($_GET["nome"]) >= 3) && (str_contains($_GET["email"],"@"))) {
+                if ((strlen($_GET["nome"]) >= 3) && (str_contains($_GET["email"],"@")) && (str_contains($_GET["email"],"."))) {
                     echo "Accesso riuscito";
                 } else {
                     echo "Accesso negato";
