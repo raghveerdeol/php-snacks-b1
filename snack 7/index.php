32fbdd2228dd185e3487d4822c945c9d6ae99754
raghveerdeol <deolraghveer5@gmail.com>
2024-06-27 12:30:56 +0200

    importato file

---
+++ snack 7/index.php
@@ -0,0 +1,10 @@
+<!-- Snack 7
+Creare un array contenente qualche alunno di un'ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Questo array dovra' essere inserito in un file separato,
+da importare.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+<?php 
+    include_once __DIR__ . "/students.php";
+    var_dump($classe);
+?>
---
+++ snack 7/students.php
@@ -0,0 +1,31 @@
+<?php 
+$classe = [
+    [
+        "nome" => "ugo",
+        "cognome" => "unto",
+        "voti" => [
+            "math" => "6",
+            "storia" => "8",
+            "fisica" => "7"
+        ],
+    ],
+    [
+        "nome" => "paolo",
+        "cognome" => "bitta",
+        "voti" => [
+                "math" => "8",
+                "storia" => "3",
+                "fisica" => "10"
+            ],
+    ],
+    [
+        "nome" => "luca",
+        "cognome" => "lugano",
+        "voti" => [
+                "math" => "5",
+                "storia" => "9",
+                "fisica" => "7"
+            ],
+    ],
+]
+?>
