f2471ad11ea4a6f48f434d07e110b1b9763966fb
raghveerdeol <deolraghveer5@gmail.com>
2024-07-03 12:31:44 +0200

    creato membership e utentePremium

--- snack 9/classi/membership.php
+++ snack 9/classi/membership.php
@@ -3,7 +3,18 @@
 class membership extends Utente {
     private $membership;
 
+    public function __construct($membership,$nome,$cognome){
+        parent::__construct($nome,$cognome);
+        $this->membership=$membership;
+    }
     
+    public function getMembership(){
+        $this->membership=$membership;
+    }
+
+    public function setMembership($membership){
+        $this->membership=$membership;
+    }
 }
 
 ?>
--- snack 9/classi/utentePremium.php
+++ snack 9/classi/utentePremium.php
@@ -1,7 +1,13 @@
 <?php 
 
-class utentePremium extends Utente {
+class utentePremium extends membership {
     
+    private $level;
+
+    public function __construct($level,$membership,$nome,$cognome){
+        parent::__construct($membership,$nome,$cognome);
+        $this->level=$level;
+    }
 
 }
 
